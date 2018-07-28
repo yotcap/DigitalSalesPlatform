279b62e21bc335de3982cf1657b91c469c4471ce
kaibing <2235709209@qq.com>
2018-07-28 20:13:27 +0800

    添加登录检测

--- src/main/java/com/yotcap/interceptor/LoginInterceptor.java
+++ src/main/java/com/yotcap/interceptor/LoginInterceptor.java
@@ -1,4 +1,27 @@
 package com.yotcap.interceptor;
 
-public class LoginInterceptor {
+import com.yotcap.pojo.User;
+import com.yotcap.result.Const;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    /**
+     * 判断是否登录
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) request.getSession().getAttribute(Const.CURRENT_USER);
+        if (user==null){
+            return false;
+        }
+        return super.preHandle(request, response, handler);
+    }
 }
